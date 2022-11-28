ecdbb971ccd8b0ecaa5ff9aba17707f4b0fb4505
Nurullah Guguk <76129770+nurullahguguk@users.noreply.github.com>
2022-11-29 00:43:54 +0300

    Core Layer is completed successfully.

---
+++ Ebebul.Core/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebebul.Core.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        //Aşağıdaki tanımlama "Navigation Property" olarak geçmektedir. Categoryiden'dan User'a gidebilirim.
+        //Bir Categorinin birden çok userı vardır.
+        public ICollection<User> Users { get; set; }
+    }
+}
